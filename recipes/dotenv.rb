06569e76512df6d230993da3103517af9acc5e18
Yuriy Padlyak <yuriypadlyak@gmail.com>
2015-09-30 17:27:41 +0300

    fix .env file creation

--- recipes/dotenv.rb
+++ recipes/dotenv.rb
@@ -15,13 +15,18 @@ require 'shellwords'
 node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
 
-  Chef::Log.info("Generating dotenv for app: #{application} with env: #{rails_env}...")
-  
+  Chef::Log.info("Generating dotenv for app: #{application} with env: #{rails_env}... in #{deploy[:deploy_to]}")
+
+  require 'yaml'
+
   environment_variables = deploy[:app_env].to_h.merge(deploy[:environment_variables].to_h)
-  open("#{deploy[:deploy_to]}/shared/.env", 'w') do |f|
-    require 'yaml'
-    environment_variables.each do |name, value|
-      f.puts "#{name}=#{value.to_s.shellescape}"
-    end
-  end  
+
+  env_file_content = environment_variables.map{|name,value| "#{name}=#{value.to_s.shellescape}"}.join("\n")
+
+  file "#{deploy[:deploy_to]}/shared/.env" do
+    content env_file_content
+    owner "deploy"
+    group  "www-data"
+    mode 00774
+  end
 end
